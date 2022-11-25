0f74d1d7f05d317f7fac44b22833a05ac83cbf39
Linda Busra Berg <114346046+LindaBusra@users.noreply.github.com>
2022-11-25 18:29:23 +0100

    AutoWidening

---
+++ src/main/java/JavaLessons/day02_typecasting_scanner/AutoWidening.java
@@ -0,0 +1,29 @@
+package JavaLessons.day02_typecasting_scanner;
+
+public class AutoWidening {
+    public static void main(String[] args) {
+
+
+
+        byte num1 = 23;
+        short num2 = 55;
+
+        int num3 = num1 + num2 ;
+        System.out.println("num3: " +num3);     // result is int 78
+
+        double num4 = num1 * num2;  // result is double -->1265.0
+
+        num4 = num2 / num1;
+        System.out.println("num4 : " + num4);  //result was 2.391, num1 and num2 are int. result->2, num4 is double-->2.0
+
+
+        double num5;
+        num5 = (double)num2/num1;   //   55.0/23 = 2.391
+        System.out.println("num5: " + num5);
+
+        double num6 = (double)(num2/num1);  //2.0
+        System.out.println("num6: " + num6);
+
+    }
+
+}

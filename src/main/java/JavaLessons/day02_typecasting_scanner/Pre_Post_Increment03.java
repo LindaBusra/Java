2bcd8b2cbbb52f9e350a87e7d927e24a1d805e88
Linda Busra Berg <114346046+LindaBusra@users.noreply.github.com>
2022-11-26 11:16:15 +0100

    Pre_Post_Increment

---
+++ src/main/java/JavaLessons/day02_typecasting_scanner/Pre_Post_Increment03.java
@@ -0,0 +1,65 @@
+package JavaLessons.day02_typecasting_scanner;
+
+public class Pre_Post_Increment03 {
+
+    public static void main(String[] args) {
+
+
+
+
+//Question 1- Print final value of a, b, c, d in the console
+
+        int a = 5;
+        int b = 2;
+        int c;
+        int d;
+        c = ++b;        //c=3;  b=3
+        d = a++;        //d=5;  a=6
+        c++;        //c=4
+
+
+
+        System.out.println("a: "+a+"\nb: " + b + "\nc: " + c + "\nd: " + d);
+
+
+
+//-------------------------------------------------------------------------------------------------
+
+
+////Question 2- What is the output?
+
+        int num1=9;
+        int num2=num1++;
+
+        if(num2<10)    {
+            System.out.println(num2 + " Hello World");
+        } else {
+            System.out.println(num1 + "Hello Universel");
+        }
+
+
+
+//-------------------------------------------------------------------------------------------------
+
+
+//Question 3- What is the result of e+f+g+h ?
+
+
+        int e = 10;
+        int f = e + 1;        //num2=11
+
+        f += 5;          //num2=16
+        int g =  f++;     //num3=16, num2=17
+        int h = ++e;      //num4=11       num1=11
+
+        System.out.println(e + f + g + h);
+
+
+
+
+
+
+
+    }
+
+}

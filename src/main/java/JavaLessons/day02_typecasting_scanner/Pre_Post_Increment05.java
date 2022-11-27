23537471c91614962628f09b27bf794cae677740
Linda Busra Berg <114346046+LindaBusra@users.noreply.github.com>
2022-11-27 13:40:26 +0100

    Pre_Pots_Increment

---
+++ src/main/java/JavaLessons/day02_typecasting_scanner/Pre_Post_Increment05.java
@@ -0,0 +1,42 @@
+package JavaLessons.day02_typecasting_scanner;
+
+public class Pre_Post_Increment05 {
+    public static void main(String[] args) {
+
+     int a = 12;
+
+     int b = a++;       //Post increment
+
+        System.out.println(b);      //12
+        System.out.println(a);      //13
+
+
+        int x = 15;
+        int y = x--;        //Post Decremenmt
+        System.out.println(x);
+        System.out.println(y);
+
+
+        int m = 21;
+        int n = ++m;        //Pre Increment
+        System.out.println(m);      //22
+        System.out.println(n);      //22
+
+
+        int t = 21;
+        int u = --m;        //Pre Decrement
+        System.out.println(m);      //21
+        System.out.println(n);      //22
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

6668b87676288b6a2b94ae32ec2cb5dc95541286
Linda Busra Berg <114346046+LindaBusra@users.noreply.github.com>
2022-11-28 18:15:02 +0100

    Scanner

---
+++ src/main/java/JavaLessons/day02_typecasting_scanner/Scanner04.java
@@ -0,0 +1,53 @@
+package JavaLessons.day02_typecasting_scanner;
+
+import java.util.Scanner;
+
+public class Scanner04 {
+
+    public static void main(String[] args) {
+
+
+
+
+/*Example 1- Ask user to enter width, height and the leghth of a rectangular
+prism and then calculate the total surface area?
+Dikdörtgenler prizmasının yüzey alanı ve hacmi   Alan=2(ab+bc+ac)  Hacim=a.b.c      */
+
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is the length, width and height of rectangular prism");
+        Double length = scan.nextDouble();
+        Double width= scan.nextDouble();
+        Double height= scan.nextDouble();
+
+        System.out.println("The total surface area of rectangular prism is " + (2*length*width+2*length*height+2*width*height));
+        System.out.println("The volume of rectangular prism is " + width*length*height);
+
+
+
+//----------------------------------------------------------------------------
+
+
+
+//Example 2 -Get the width and the length of rectangle forum user and calculate the area
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the length of rectangle?");
+        double length1 = input .nextDouble();
+        System.out.println("What is the width of rectangle?");
+        double width1= input .nextDouble();
+
+        System.out.println("The area of rectangle is " + (length1*width1));
+
+
+
+
+
+
+
+
+
+    }
+}

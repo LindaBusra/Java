125ca60b67f4a984710866db51bf9612c97ce34a
Linda Busra Berg <114346046+LindaBusra@users.noreply.github.com>
2022-12-04 21:01:55 +0100

    Concatenation

---
+++ src/main/java/JavaLessons/day05_concatenation_modulus_nestedternary/Concatenation02.java
@@ -0,0 +1,45 @@
+package JavaLessons.day05_concatenation_modulus_nestedternary;
+
+import java.util.StringJoiner;
+
+public class Concatenation02 {
+
+    public static void main(String[] args) {
+
+
+        String s=50+30+"Einar"+40+40;
+        System.out.println(s);      //80Einar4040
+
+
+        String s1="Ole & ";
+        String s2="Carina";
+        String s3=s1.concat(s2);
+        System.out.println(s3); // Ole & Carina
+
+
+        //String concatenation using StringBuilder class
+        StringBuilder a = new StringBuilder("Welcome");    //String 1
+        StringBuilder b = new StringBuilder(" our house");    //String 2
+        StringBuilder c = a.append(b);   //String 3 to store the result
+        System.out.println(c.toString());  //Displays result
+
+        //In the above code snippet, s1, s2 and s are declared as objects of StringBuilder class.
+        // s stores the result of concatenation of s1 and s2 using append() method.
+
+
+
+        //String concatenation using String.join() method
+        String d = new String("Hello");    //String 1
+        String e = new String(" again");    //String 2
+        String f = String.join("",d,e);   //String 3 to store the result
+        System.out.println(f.toString());
+
+
+        //String concatenation using StringJoiner class
+        StringJoiner g = new StringJoiner(", ");   //StringeJoiner object
+        g.add("Hello");    //String 1
+        g.add("World");    //String 2
+        System.out.println(g.toString());  //Displays result
+
+    }
+}

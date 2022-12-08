5fe5a124d93ff99fc00c829236701bf6c15c3bbe
Linda Busra Berg <114346046+LindaBusra@users.noreply.github.com>
2022-12-08 15:41:36 +0100

    Methods

---
+++ src/main/java/JavaLessons/day06_methods/Method02.java
@@ -0,0 +1,16 @@
+package JavaLessons.day06_stringmanipulations_methods;
+
+public class Method02 {
+    public static void main(String[] args) {
+
+        System.out.println(addIntegers(25,56));
+        System.out.println(addIntegers(2,9));
+
+    }
+
+
+    public static int addIntegers (int a, int b) {
+
+        return a+b;
+    }
+}

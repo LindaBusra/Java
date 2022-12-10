263d5846e99516ecf32959c87345b8fc9dc73d62
Linda Busra Berg <114346046+LindaBusra@users.noreply.github.com>
2022-12-10 22:30:45 +0100

    StringManipulations

---
+++ src/main/java/JavaLessons/day07_stringmanipulations/StringManipulations04.java
@@ -0,0 +1,50 @@
+package JavaLessons.day07_stringmanipulations;
+
+public class StringManipulations04 {
+    public static void main(String[] args) {
+
+
+
+
+        String str = "Java is easy.";
+
+        boolean isExist = str.contains("va");
+        System.out.println(isExist);
+
+        boolean isExist1 = str.contains("x");
+        System.out.println(isExist1);
+
+        boolean startsWith = str.startsWith("J");
+        System.out.println(startsWith);
+
+        System.out.println(str.startsWith("Java"));   //true
+
+        System.out.println(str.startsWith("a",9)); //true (9.karakterden sonra a ile basliyor)
+        //skip first 9 characters
+
+        System.out.println(str.startsWith("i", 5));
+        //skip first 5 characters
+
+
+
+
+//----------------endsWith-------------------------------------
+
+
+        String s = "Do practice to be better";
+        boolean rslt = s.endsWith("r");
+        System.out.println(rslt);  //true
+        System.out.println(s.endsWith("better"));
+
+
+
+//----------------concat-------------------------------------
+
+
+        //concat() method is some with the "+" in Java.
+
+        System.out.println(s.concat("!"));  //Do practice to be better
+
+        System.out.println("".concat("!").concat(s));
+    }
+}

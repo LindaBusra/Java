2fa5ddcab5f1e67882a4a0de84705eb1a7eca5b6
Linda Busra Berg <114346046+LindaBusra@users.noreply.github.com>
2022-12-10 22:29:20 +0100

    StringManipulations

---
+++ src/main/java/JavaLessons/day07_stringmanipulations/StringManipulations02.java
@@ -0,0 +1,55 @@
+package JavaLessons.day07_stringmanipulations;
+
+public class StringManipulations02 {
+    public static void main(String[] args) {
+
+
+
+
+//Example 1- Type code to se the order number of the first accurence of the character 'a'
+// "I want to learn Java"  --->
+
+
+        String str = "I want to learn Java";
+        int a = str.indexOf('a'); //indexOf() method gives you the index of first accurance of a spesific character
+        System.out.println(a+1);  //order number is one more that, because order number start from 1
+
+
+
+//-------------------------------------------------------------------------------------------
+
+
+
+//Example 2: Type code to see the order number of the first occurrence of "Java"
+
+
+        String s = "I like to study Java, learn Java, earn money, Java is easy.";
+        int b = s.indexOf("Java");   //16: it is index, basladigi yerin indeksini verir
+        System.out.println(b+1);    //order number
+        System.out.println(s.indexOf("*"));
+
+
+//Note 1:indexOf() method can be used with char and String data types like indexOf('a') and indexOf("Java")
+//Note 2:if you use string inside the indexOf(), it will return the index of the first character, indexOf("Java")
+//returns indexOf "J"
+//Note 3:s.indexOf("xyz"); ==>-1   indexOf() method returns "-1" for non-existing character
+
+
+
+//-------------------------------------------------------------------------------------------
+
+
+//Example 3 : Type code to check if String has 'a' in it or not. Print appropriate messages for the senarious.
+
+        String word = "I like to study";
+
+        if (word.indexOf('a')==-1) {
+            System.out.println("The word has not character a");
+        } else {
+            System.out.println("The word has character a");
+        }
+
+
+
+    }
+}

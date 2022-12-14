0cba59564013204397380bd806ede961307d193e
Linda Busra Berg <114346046+LindaBusra@users.noreply.github.com>
2022-12-14 19:33:07 +0100

    DoWhileLoop

---
+++ src/main/java/JavaLessons/day10_DoWhileLoop/DoWhile01.java
@@ -0,0 +1,87 @@
+package JavaLessons.day10_DoWhileLoop;
+
+public class DoWhile01 {
+    public static void main(String[] args) {
+
+
+
+//Example 1- To write five times "Hello World" by using do-while
+
+
+        // Declaring and initialization expression
+        int i = 1;
+
+        // Do-while loop
+        do {
+
+            // Body of do-while loop
+            // Print statement
+            System.out.println("Hello World");
+
+            // Update expression
+            i++;
+        }
+
+        // Test expression
+        while (i < 6);
+
+        System.out.println();
+
+
+
+//---------------------------------------------------------------------------------------------------------------
+
+
+
+//Example 2- Find the sum of number from 11 to 21 by using do-while
+
+        int x = 21, sum = 0;
+
+
+        do {
+            sum += x;
+            x--;
+
+        }  while (x > 10);
+
+
+
+        System.out.println("The sum of the numbers: " + sum);
+
+
+
+//---------------------------------------------------------------------------------------------------------------
+
+
+
+//Example 3- Find the digits of number by using do-while
+
+
+   int numb =1234;
+
+   int sumt = 0;
+
+
+   do{
+       sumt = sumt + numb % 10;
+       numb = numb / 10;
+
+   } while(numb > 0);
+
+        System.out.println("The digits of number: " + sumt);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

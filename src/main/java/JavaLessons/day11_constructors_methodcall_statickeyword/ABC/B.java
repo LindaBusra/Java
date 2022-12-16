aa0dc8ca1d6bc6dcac39965b7159af9510fe2042
Linda Busra Berg <114346046+LindaBusra@users.noreply.github.com>
2022-12-16 18:00:21 +0100

    Constructors-Methodcall-StaticKeyword

---
+++ src/main/java/JavaLessons/day11_constructors_methodcall_statickeyword/ABC/B.java
@@ -0,0 +1,9 @@
+package JavaLessons.day11_constructors_methodcall_statickeyword.ABC;
+
+public class B extends A {
+
+    String s = "Class B";
+    {
+        System.out.println(super.s);
+    }
+}

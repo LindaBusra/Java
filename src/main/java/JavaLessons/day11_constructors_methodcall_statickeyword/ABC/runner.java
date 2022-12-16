96f290909d620a17146fa1fcce7e3f3b2f204f21
Linda Busra Berg <114346046+LindaBusra@users.noreply.github.com>
2022-12-16 18:01:21 +0100

    Constructors-Methodcall-StaticKeyword

---
+++ src/main/java/JavaLessons/day11_constructors_methodcall_statickeyword/ABC/runner.java
@@ -0,0 +1,11 @@
+package JavaLessons.day11_constructors_methodcall_statickeyword.ABC;
+
+public class runner {
+    public static void main(String[] args) {
+
+
+        C c = new C();
+        System.out.println(c.s);
+    }
+
+}

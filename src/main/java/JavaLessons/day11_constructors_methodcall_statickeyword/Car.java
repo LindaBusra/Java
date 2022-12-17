9dc8580936e5b6bf5ef1aea5e348a75f1bc4f8ce
Linda Busra Berg <114346046+LindaBusra@users.noreply.github.com>
2022-12-17 23:11:17 +0100

    Constructors_Methodcall_StaticKeyword

---
+++ src/main/java/JavaLessons/day11_constructors_methodcall_statickeyword/Car.java
@@ -0,0 +1,51 @@
+package JavaLessons.day11_constructors_methodcall_statickeyword;
+
+public class Car {
+
+
+/*When you create a class you will use it to create an object most probably.
+because of that when you create a class Java puts constructor in it automatically.
+Its name is default constructors are invisible.
+
+If you create your own constructor, java deletes the "Default Constructor";
+
+How to create constructor?
+-Constructor name must match with the class name.
+
+What is the different between "Constructors and methods"
+1-Constructors do not have return type but methods have.
+2-Constructor name must match with the class name buts method name can be any name.
+3-Constructor are used to create object but methods are used to create some actions.
+
+
+*/
+    String make = "Toyota";
+    String model = "Prius";
+    int year = 2020;
+    int price =20000;
+
+
+    public Car() {
+
+        System.out.println("Car constructor 1");
+
+            }
+
+    public Car(String make,String model,int year, int price ) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+
+
+    }
+
+    public Car(String make,String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+
+    }
+
+    }
+

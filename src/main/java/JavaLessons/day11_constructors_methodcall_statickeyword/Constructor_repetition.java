0425bb78e6fa3fb1b82141918ffb5c7ad4510636
Linda Busra Berg <114346046+LindaBusra@users.noreply.github.com>
2022-12-17 23:13:13 +0100

    Constructors_Methodcall_StaticKeyword

---
+++ src/main/java/JavaLessons/day11_constructors_methodcall_statickeyword/Constructor_repetition.java
@@ -0,0 +1,36 @@
+package JavaLessons.day11_constructors_methodcall_statickeyword;
+
+import java.util.Scanner;
+
+public class Constructor_repetition {
+    public static void main(String[] args) {
+
+
+//To create object we need classes. Object is instens of class.
+//Constructor are used to have object from class.
+//To create object vwe have to have constructor.
+
+
+//Java knows classes are template to create object. Because of that, Java puts constructor in every class
+//as default.   Default constructors are invisible inside the classes.
+//Default constructors have no parameter inside parenthesis.
+//Constructor names must be same with the class name.
+
+
+
+//How to create object?
+ Scanner scan = new Scanner(System.in); // (System.in) it is not default, java created is specifically.
+    //Scanner(class name) scan(obj name) = new (keyword)  ClassName
+
+ Constructor_repetition obj1      = new Constructor_repetition();    //Class name and Constructor name is the same
+// class name   obj name      keyword   Constructor name        //if it is default ();
+
+
+
+
+
+
+
+
+    }
+}

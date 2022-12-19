41fd2c8ea98fc485fecb7cd6fd14f29681ff814a
Linda Busra Berg <114346046+LindaBusra@users.noreply.github.com>
2022-12-19 17:05:50 +0100

    StaticBlock

---
+++ src/main/java/JavaLessons/day12_StaticBlock/MyClass.java
@@ -0,0 +1,32 @@
+package JavaLessons.day12_StaticBlock;
+
+public class MyClass {
+
+    static int num1;
+    static String name = "Ole";
+
+
+
+    public static void main(String[] args) {
+
+        add();
+        product(12);
+
+    }
+
+    public static void add() {                       //method
+        num1++;
+        int num2=6;
+        char letter;                            //local variable
+        System.out.println("Do addition to " + name );
+        System.out.println(num1+num2);
+    }
+
+    public static void product(int num3){                   //method
+        name = "Veli";
+        int num2 = 7;                               //local variable
+        System.out.println(name);
+        System.out.println(num3*num1*num2);
+    }
+
+}

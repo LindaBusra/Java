82ee0e7187f9975695991e98553635d8dd995fe4
Linda Busra Berg <114346046+LindaBusra@users.noreply.github.com>
2022-12-30 16:42:48 +0100

    AccessModifiers_Inheritance

---
+++ src/main/java/JavaLessons/day20_accessmodifiersinheritance/Example2/Bicycle.java
@@ -0,0 +1,9 @@
+package JavaLessons.day20_accessmodifiersinheritance.Example2;
+
+public class Bicycle extends Vehicle{
+
+    public Bicycle(String make){
+        super("Cu");
+        System.out.println("Bicycle constructor with parameter");
+    }
+}

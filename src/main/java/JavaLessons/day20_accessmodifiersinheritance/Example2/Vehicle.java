6ea52c6eb7ed3829225fa0ad550745a884701a88
Linda Busra Berg <114346046+LindaBusra@users.noreply.github.com>
2023-01-01 00:06:54 +0100

    AccessModifiers_Inheritance

---
+++ src/main/java/JavaLessons/day20_accessmodifiersinheritance/Example2/Vehicle.java
@@ -0,0 +1,16 @@
+package JavaLessons.day20_accessmodifiersinheritance.Example2;
+
+public class Vehicle {
+
+
+//    public Vehicle(){
+//
+//        System.out.println("Vehicle constructor without parameter");
+//    }
+
+    public Vehicle(String note){
+
+        System.out.println("Vehicle constructor with String parameter");
+    }
+
+}

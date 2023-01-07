d1ef5363d2371c5b95cdd97dd19962bd3f14afe6
Linda Busra Berg <114346046+LindaBusra@users.noreply.github.com>
2023-01-07 06:15:48 +0100

    Exceptions

---
+++ src/main/java/JavaLessons/day27_exceptions/E01.java
@@ -0,0 +1,25 @@
+package JavaLessons.day27_exceptions;
+
+public class E01 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+//Write a program for example of try  and catch block. In this example check whether the given
+//array size is negative or not.
+
+        try {
+
+            int[] arr = new int[-4];
+
+            System.out.println(arr.length);
+
+        } catch (NegativeArraySizeException e) {
+            System.out.println("Array size can not be negative");
+        }
+
+        Thread.sleep(2000);  // it stopped the system 2 seconds.
+
+
+    }
+    }

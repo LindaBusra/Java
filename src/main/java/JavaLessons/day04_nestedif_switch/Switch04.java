db38dbd748f32fed9f47cfce600fbf694fe66b0a
LindaBusra <linda.busra.berg@gmail.com>
2023-01-28 21:34:58 +0100

    Switch

---
+++ src/main/java/JavaLessons/day04_nestedif_switch/Switch04.java
@@ -0,0 +1,77 @@
+package JavaLessons.day04_nestedif_switch;
+
+import java.util.Scanner;
+
+public class Switch04 {
+
+    public static void main(String[] args) {
+
+
+
+ //Switch question...
+
+
+        Integer age = 18;
+        switch (age)
+        {
+            case (16):
+                System.out.println("You are under 18.");
+                break;
+            case (18):
+                System.out.println("You are eligible for vote.");
+                break;
+            case (65):
+                System.out.println("You are senior citizen.");
+                break;
+            default:
+                System.out.println("Please give the valid age.");
+
+        }
+
+//---------------------------------------------------------------
+
+
+        //Type Weekend or Weekday
+
+        Scanner scan =  new Scanner(System.in);
+        System.out.println("what day of the week is today?");
+        byte gun = scan.nextByte();
+
+        switch(gun) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Weekday");
+                break;
+            case 6:
+            case 7:
+                System.out.println("Weekend");
+                break;
+            default:
+                System.out.println("Invalid input");
+                break;
+        }
+
+
+
+//------------------------------------------------------------------------------------------------
+
+
+        int day = 4;
+        switch (day) {
+            case 6:
+                System.out.println("Today is Saturday");
+                break;
+            case 7:
+                System.out.println("Today is Sunday");
+                break;
+            default:
+                System.out.println("Looking forward to the Weekend");
+        }
+
+
+
+    }
+}

1b45896d863e3f8cc36a0de2efd79faad5494c0b
LindaBusra <linda.busra.berg@gmail.com>
2023-01-28 21:35:18 +0100

    Switch

---
+++ src/main/java/JavaLessons/day04_nestedif_switch/Switch05.java
@@ -0,0 +1,67 @@
+package JavaLessons.day04_nestedif_switch;
+
+public class Switch05 {
+
+    public static void main(String[] args) {
+
+
+        //ATM ornegi
+
+//        Scanner scan = new Scanner(System.in);
+//        int bakiye = 1000;
+//
+//        System.out.println("yapmak istediginiz islemi seciniz");
+//        System.out.println("1: Bakiye goruntule");
+//        System.out.println("2: Para yatirma");
+//        System.out.println("3: Para cekme");
+//        System.out.println("4: Sistemden cikis");
+//
+//        int islem = scan.nextInt();
+//        switch (islem) {
+//            case 1:
+//                System.out.println("Bakiyeniz " + bakiye + " TL dir");
+//                break;
+//            case 2:
+//                System.out.println("ne kadar para yatiracksiniz? ");
+//                int miktar = scan.nextInt();
+//                bakiye = bakiye + miktar;
+//                System.out.println("Son bakiyeniz " +  bakiye) ;
+//                break;
+//            case 3:
+//                System.out.println("ne kadar para cekeceksiniz? ");
+//                int cekilecek = scan.nextInt();
+//                bakiye = bakiye - cekilecek;
+//                System.out.println("Son bakiyeniz " +  bakiye) ;
+//                break;
+//            case 4:
+//                System.out.println("Sistemden cikiliyor...");
+//                break;
+//            default:
+//                System.out.println("Gecersiz islem");
+//        }
+
+
+/*Type java code by using switch statement.
+        A school has following rules for grading system:
+1. For 50 - C 2. For 80 - B. 4. For 100 - A
+Ask user to enter marks and print the corresponding grade.
+ */
+
+//        int a = 85;
+//
+//        switch(a) {
+//            case 50:
+//                System.out.println("Grade is C");
+//                break;
+//            case 80:
+//                System.out.println("Grade is B");
+//                break;
+//            case 100:
+//                System.out.println("Grade is A" + a);
+//                break;
+//            default:
+//                System.out.println("Invalid");
+//        }
+
+    }
+}

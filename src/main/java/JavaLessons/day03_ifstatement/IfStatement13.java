195e0d38cd67315d7c159151ae3789bd699253b6
LindaBusra <linda.busra.berg@gmail.com>
2023-02-10 19:35:23 +0100

    IfStatement

---
+++ src/main/java/JavaLessons/day03_ifstatement/IfStatement13.java
@@ -0,0 +1,43 @@
+package JavaLessons.day03_ifstatement;
+
+import java.util.Scanner;
+
+public class IfStatement13 {
+
+    public static void main(String[] args) {
+
+
+
+
+//TASK : Write a code that prints in which region the coordinate point you received from the user is
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter x point");
+        int x = scanner.nextInt();
+
+        System.out.println("Enter y point");
+        int y = scanner.nextInt();
+
+        if (x > 0 && y > 0) {
+            System.out.println("value that you input belongs to first region");
+        }
+        else if (x < 0 && y > 0) {
+            System.out.println("value that you input belongs to second region");
+        } else if (x < 0 && y < 0) {
+            System.out.println("value that you input belongs to third region");
+        } else if (x > 0 && y < 0) {
+            System.out.println("value that you input belongs to fourth region");
+        } else if (x != 0 && y == 0) {
+            System.out.println("value that you input belongs to x point");
+        } else if (x == 0 && y != 0) {
+            System.out.println("value that you input belongs to y point");
+        } else {
+            System.out.println("It is in origin");
+        }
+
+
+
+
+    }
+}

c36b6bd2478dfa6c22e829b69b145c2ff4dd728c
LindaBusra <linda.busra.berg@gmail.com>
2023-02-11 21:44:47 +0100

    StringManipulations

---
+++ src/main/java/JavaLessons/day07_stringmanipulations/StringManipulations07.java
@@ -0,0 +1,46 @@
+package JavaLessons.day07_stringmanipulations;
+
+public class StringManipulations07 {
+    public static void main(String[] args) {
+
+
+
+        String str = "Java is easy, Java is OOP.";
+
+        //Replacefirst changes just the first one
+
+        System.out.println(str.replaceFirst("Java", "Apex"));
+
+        //Replace changes everyone
+
+        System.out.println(str.replace("Java", "Apex"));
+
+
+
+//------------------------------------------------------------------------------------------------------------
+
+
+
+ /*   String shirtPrice = ‘$12.99’;
+      String bookPrice = ‘$35.99’;
+      Type code to find the sum of the shirt and book prices.
+  */
+        String shirtPrice = "$12.99";
+        String bookPrice = "$35.99";
+
+        String shirt = shirtPrice.replace("$", "");
+        String book = bookPrice.replace("$", "");
+
+        //valueOf() method converst Strings data type to numeric data type
+        //Note:To be able to use valueOf() method, String must be in numeric format
+        double sP = Double.valueOf(shirt);
+        double bP = Double.valueOf(book);
+
+        System.out.println("The sum of the shirt and book prices: " + (sP + bP));
+
+
+
+
+
+    }
+}

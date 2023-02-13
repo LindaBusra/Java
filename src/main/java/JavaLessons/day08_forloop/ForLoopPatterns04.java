b2c00142b15f30770987f1e3f046b98dd08249f0
LindaBusra <linda.busra.berg@gmail.com>
2023-02-13 22:29:17 +0100

    ForLoopPatterns

---
+++ src/main/java/JavaLessons/day08_forloop/ForLoopPatterns04.java
@@ -0,0 +1,57 @@
+package JavaLessons.day08_forloop;
+
+public class ForLoopPatterns04 {
+
+    public static void main(String[] args) {
+
+
+
+/* Example 1 - Type code for this pattern
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+ */
+
+
+        int row = 5;
+
+        for(int i=0; i<row; i++) {
+
+            for (int j = 1; j <= i + 1; j++) {
+
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+
+
+
+
+/* Example  -Type code for this pattern
+
+
+        1
+        2 1
+        3 2 1
+        4 3 2 1
+        5 4 3 2 1          */
+
+
+        int row1 =5;
+
+        for(int i=1; i<=row1; i++) {
+            for (int j = i; j >=1; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}

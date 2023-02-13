8f8e473642b94bb4d2520862aa167af07f1e89f8
LindaBusra <linda.busra.berg@gmail.com>
2023-02-13 22:28:45 +0100

    ForLoopPatterns

---
+++ src/main/java/JavaLessons/day08_forloop/ForLoopPatterns03.java
@@ -0,0 +1,55 @@
+package JavaLessons.day08_forloop;
+
+public class ForLoopPatterns03 {
+
+    public static void main(String[] args) {
+
+
+
+/*Example 1 - Type code to draw the following image by using a for loop.
+
+
+A A A A A
+A A A A A
+A A A A A           */
+
+
+        int row = 3;
+        for(int i=1; i<4; i++ ) {
+
+            for(int j=1; j<row+3; j++) {
+
+                System.out.print("A " );
+            }
+            System.out.println();
+        }
+
+
+
+
+
+/*Example 2 -  Type code to draw the following image by using a for loop.
+
+
+
+ */
+        /* Pattern
+
+         *
+         * *
+         * * *
+         * * * *
+         * * * * *
+
+         */
+
+        for (int i = 1; i < 6; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}

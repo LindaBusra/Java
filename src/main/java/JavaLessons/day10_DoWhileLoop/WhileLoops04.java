30cfa50b44d1bbfc8e31bbc0909c6a6ba7c1ceb8
LindaBusra <linda.busra.berg@gmail.com>
2023-02-15 16:24:13 +0100

    WhileLoop

---
+++ src/main/java/JavaLessons/day10_DoWhileLoop/WhileLoops04.java
@@ -0,0 +1,64 @@
+package JavaLessons.day10_DoWhileLoop;
+
+import java.util.Scanner;
+
+public class WhileLoops04 {
+    public static void main(String[] args) {
+
+
+
+
+
+// Example 1- Java program to count the number of digits available in a given number using while loop.
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter the number");
+        int number = scan.nextInt();
+
+        int digit = 0;
+
+        while(number != 0) {
+            digit++;
+            number = number/10;
+        }
+
+        System.out.println("The total number if digits is " + digit);    // for 1589 -->4
+
+
+//------------------------------------------------------------------------------------------------------
+
+
+
+// Example 2-Example for break;
+
+        int a=1;
+        while(a<10) {
+            System.out.println(a);
+
+            if(a==4) {
+                break;
+            }
+            a++;
+        }
+        System.out.println();
+
+
+
+
+// Example 3- Example for continue;
+
+
+
+        for(int b =10; b<=40; b=b+10) {
+            if( b ==30) {
+             continue;
+            }
+            System.out.println(b);
+        }
+
+
+
+
+
+    }
+}

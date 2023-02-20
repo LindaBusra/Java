7f9d5097487aa130f0c1cecb2e93e8f5e8368cc3
LindaBusra <linda.busra.berg@gmail.com>
2023-02-20 22:11:14 +0100

    WhileLoops

---
+++ src/main/java/JavaLessons/day10_DoWhileLoop/WhileLoops12.java
@@ -0,0 +1,53 @@
+package JavaLessons.day10_DoWhileLoop;
+
+public class WhileLoops12 {
+
+    public static void main(String[] args) {
+
+
+
+
+// Example 1- Find factorial of given number.
+
+        int num = 5;
+
+        int f = 1;
+
+        while(num>0) {
+
+            f = f * num;
+            num--;
+        }
+
+        System.out.println("the factorial is: " + f);
+
+
+
+//---------------------------------------------------------------------------------------------------------
+
+
+// Example 2- Find sum of all dividers of guven number
+
+
+
+        int number = 60;
+        int divider = 1;
+        int sumOfDivider = 0;
+
+
+        while(number>0) {
+
+            if(number%divider ==0) {
+                System.out.println(divider  + " is divider of " + number);
+                sumOfDivider += divider;
+            }
+
+            divider++;
+        }
+
+        System.out.println("The sum of all dividers is: " + sumOfDivider);
+
+
+
+    }
+}

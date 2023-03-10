696616cbc07132e30588dbb8aaba31f211e942ef
LindaBusra <linda.busra.berg@gmail.com>
2023-03-10 21:22:58 +0100

    Exceptions

---
+++ src/main/java/JavaLessons/day26_exceptions/E04.java
@@ -0,0 +1,21 @@
+package JavaLessons.day26_exceptions;
+
+import java.io.*;
+class E04 {
+    void myMethod(int num) throws IOException, ClassNotFoundException {
+        if (num == 1)
+            throw new IOException("IOException Occurred");
+        else
+            throw new ClassNotFoundException("ClassNotFoundException");
+    }
+
+
+    public static void main(String args[]) {
+        try {
+            E04 obj = new E04();
+            obj.myMethod(1);
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+}

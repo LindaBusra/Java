34f0c25bfa8d0c9e8fe995d6223a7d37aa472e98
LindaBusra <linda.busra.berg@gmail.com>
2023-03-19 22:36:15 +0100

    ASCII Table

--- src/main/java/JavaLessons/day01_variables/AsciiTable.java
+++ src/main/java/JavaLessons/day01_variables/AsciiTable.java
@@ -4,37 +4,43 @@ public class AsciiTable {
     public static void main(String[] args) {
 
 
+
         int num=10;
         char ch='a';
 
-        System.out.println(num+ch);   // 10+97=100
+        System.out.println(num+ch);  //10+97=107
 
 
-        System.out.println(num*ch);   //10*97=907
+        System.out.println(num*ch); //970
 
         String str="banan";
-        System.out.println(str+ch);
-        System.out.println(ch+2);
-        System.out.println(str+num+ch);
+        System.out.println(str+ch);   //banana
+        System.out.println(ch+2);   //99
+        System.out.println(str+num+ch);  //banan10a
 
         System.out.println(""+num+ch);   //10a
 
 
-        char value='1';
-        System.out.println(value+ch); // '1'+'a' ---> 49+97=146
+        char value='1';  //        System.out.println(value + 0); -->ascii 49
+
+        System.out.println(value+ch);   //49+97 = 146
 
         char result='a';
-        System.out.println(result);                     //a
-        System.out.println(num+result);             //10+97=107
-        System.out.println("banan"+result);        //banana
-        System.out.println(result*2);           //97*2=194
-        System.out.println(""+num+result);          //10a
+        System.out.println(result);  //a
+        System.out.println(num+result); //107
+        System.out.println("banan"+result);  //banana
+        System.out.println(result*2);   //97*2=194
+        System.out.println(""+num+result);  //10a
 
+
+        char cc = 'a';
+        System.out.println(String.valueOf(cc));
 
         // From String to integer
         String number = "3";
         int r1 = Integer.valueOf(number);
         int r2 = Integer.parseInt(number);  //it is faster
+
 
 
 

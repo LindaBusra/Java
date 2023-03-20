b0861592e197150cc30d0811201368ff4ef82990
LindaBusra <linda.busra.berg@gmail.com>
2023-03-20 22:23:18 +0100

    Variables

--- src/main/java/JavaLessons/day01_variables/Variables02.java
+++ src/main/java/JavaLessons/day01_variables/Variables02.java
@@ -32,6 +32,41 @@ public class Variables02 {
         System.out.println(age); // 12
         System.out.println(initial);
 
+        //not
+        System.out.println("-----------------");
+        char ch = 66;
+        System.out.println(ch);             //B
+        System.out.println(ch + "cde");     //Bcde
+        System.out.println(ch + 'c');       // 66 + 99 = 165
+        System.out.println(ch + 3);         //69
+
+
+
+        char chh = 98;   //b
+
+        System.out.println(chh);  //b
+        System.out.println(chh + 1);  //99
+        System.out.println(chh + 'c'); //98+99 = 197
+        System.out.println(chh + "d"); // bd
+
+
+        char chr = 'A';
+        System.out.println(chr);     //A
+        System.out.println(chr +1);  //66
+        System.out.println(chr + 'B');   //65+66=131
+        System.out.println(chr + "B");       //AB
+        System.out.println('A');        //A
+
+
+        //not
+        byte a = 127;
+        System.out.println(Byte.MAX_VALUE); //127
+        // byte b = a + 1;  //cast to 'byte'
+        byte b = (byte) (a +1);
+
+        System.out.println(b);      //-128
+        System.out.println(b + 1);  // -128 + 1 = -127
+        System.out.println(a+1);    //128
 
 
         // create 3 integer variables, assign values to them, print them on the console one by one
@@ -49,7 +84,7 @@ public class Variables02 {
 
 
         // We need concatenation operation to add string and integer.
-        System.out.println("The sum is: "+ i01 + i02 + i03); // "The sum is: "+121314
+        System.out.println("The sum is: "+ i01 + i02 + i03); // "The sum is: "+1596
         System.out.println("The sum is: "+( i01 + i02 + i03));
 
         int sum = i01+i02+i03;
@@ -63,29 +98,31 @@ public class Variables02 {
 
         System.out.println("The multiplication is : " + (x*y));
         System.out.println("The multiplication is : " + x*y);
+        System.out.println(20 + x*y);  //35
+        System.out.println(20/ x*y);  //12
 
         /*
             Order of Operations in Math
             1) Do the operations inside the parenthesis first
             2) Do multiplication and division
-            3) Do addition and substraction
+            3) Do addition and subtraction
         */
 
 
 
   //Example - swap
 
-        String a = "water";
-        String b = "Kool-aid";
-        String temp = "null";
+        String a1 = "water";
+        String b1 = "Kool-aid";
+        String temp1 = "null";
 
-        System.out.println(a + " " + b);
+        System.out.println(a1 + " " + b1);
 
-        temp=a;
-        a=b;
-        b=temp;
+        temp1 = a1;
+        a1 = b1;
+        b1 = temp1;
 
-        System.out.println(a + " " +b);
+        System.out.println(a1 + " " +b1);
 
 
 

e68ad5040c1ceb3bc6a602717e99aafc5a2e1aa7
LindaBusra <linda.busra.berg@gmail.com>
2023-03-21 18:50:01 +0100

    ExplicitNarrowing

--- src/main/java/JavaLessons/day02_typecasting_scanner/ExplicitNarrowing.java
+++ src/main/java/JavaLessons/day02_typecasting_scanner/ExplicitNarrowing.java
@@ -5,16 +5,13 @@ public class ExplicitNarrowing {
 
 
         int num1 = 50;
-
         short num2 = (short) num1;  //50
-
         System.out.println(num2);
 
 
 
 
-        byte num3 = (byte) num1;  //50     //byte -128 ile 127
-
+        byte num3 = (byte) num1;  //50     //byte from -128 to 127
         System.out.println(num3);  //50
 
 
@@ -23,16 +20,28 @@ public class ExplicitNarrowing {
         int num4 = 500;
 
         byte num5 = (byte) num4;
-        System.out.println(num5);  //-12
+        System.out.println(num5);  //-12--> 127+256=  383  -->I need 117   (128-117+1 = 12)-->answer is -12
 
+
+        int number1=386;
+        byte number2= (byte)number1;  //127+256=383  I need 3 more     -128 -127 -126--> answer is -126
+        System.out.println(number2);
 
 
 
         int a = 135;
+        byte b = (byte) a ;   //-128-------0-------127    we have 8 more... -128, -127, .... -121
+        System.out.println("b: " +b);   //-121
 
-        byte b = (byte) a ;   //-128--------------127
 
-        System.out.println("b: " +b);
+
+        int num=130;
+        byte number = (byte)num;
+        System.out.println(number);   //--126
+
+
+
+
 
     }
 }

d4e1bd4cfbc5cc66d5ad986ab7d65ab8e0095b22
LindaBusra <linda.busra.berg@gmail.com>
2023-03-22 22:15:34 +0100

    Pre_Post_Increment

--- src/main/java/JavaLessons/day02_typecasting_scanner/Pre_Post_Increment02.java
+++ src/main/java/JavaLessons/day02_typecasting_scanner/Pre_Post_Increment02.java
@@ -7,6 +7,7 @@ public class Pre_Post_Increment02 {
 
 
 
+
 //Question-1 : What is the final result of a, b, c, d?
 
 
@@ -15,12 +16,12 @@ public class Pre_Post_Increment02 {
         int c;
         int d;
 
-        c = ++b;            //c=3, b=3
-        d = a++;            //d=5, a=6
-        c++;                //c=4
-        b = --a;            //b=5,  a=5
-        a = --c;            //a=3, c=3
-        d = b--;            //d=5, b=4
+        c = ++b;        //c=3  b=3
+        d = a++;        //d=5; a=6
+        c++;            //c=4;
+        b = --a;        //b=5;  a=5;
+        a = --c;        //a=3; c=3;
+        d = b--;        //d=5; b=4
 
 
         System.out.println(a);      //3
@@ -34,12 +35,12 @@ public class Pre_Post_Increment02 {
 //Question-2 : What is the final result of a+b+c+y?
         int x= 10;
 
-        int y=++x;
-        int e=--x;
-        int f= x++;
-        int g= x--;
+        int y=++x;      //y=11, x=11;
+        int e=--x;      //e=10, x=10;
+        int f= x++;     //f=10, x=11;
+        int g= x--;     //g=11, x=10
 
-        System.out.println(e+f+g+y);
+        System.out.println(e+f+g+y);  //10+10+11+11=42
 
 
 

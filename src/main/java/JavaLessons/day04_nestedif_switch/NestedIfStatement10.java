a0c5c0b0b0276f4c96d5e1b95424d3e2b5939f42
LindaBusra <linda.busra.berg@gmail.com>
2023-03-26 22:40:06 +0200

    NestedIfStatement

---
+++ src/main/java/JavaLessons/day04_nestedif_switch/NestedIfStatement10.java
@@ -0,0 +1,39 @@
+package JavaLessons.day04_nestedif_switch;
+
+public class NestedIfStatement10 {
+
+    public static void main(String[] args) {
+
+
+// declaring double type variables
+
+
+        Double n1 = -1.0, n2 = 4.5, n3 = -5.3, largest;
+
+
+        if (n1 >= n2) {
+
+
+            if (n1 >= n3) {
+                largest = n1;
+            }
+
+            else {
+                largest = n3;
+            }
+        } else {
+
+
+            if (n2 >= n3) {
+                largest = n2;
+            }
+
+            else {
+                largest = n3;
+            }
+        }
+
+        System.out.println("Largest Number: " + largest);
+
+    }
+}

ba11e2215004dba0bfb76228f500a265cfbb0b45
LindaBusra <linda.busra.berg@gmail.com>
2023-03-28 22:29:52 +0200

    Methods

---
+++ src/main/java/JavaLessons/day06_methods/Methods09.java
@@ -0,0 +1,47 @@
+package JavaLessons.day06_methods;
+
+public class Methods09 {
+
+    public static void main(String[] args) {
+        add(3.0,4.0); // Output A
+        add(5, 6); // Output B
+        add(7, 8.0); // Output C
+        subtract(9, 10); //Output D
+
+        Methods09 obj = new Methods09();
+        System.out.println(obj.sumNumber(1, 2));
+        System.out.println(obj.sumNumber(1, 2, 3));
+
+    }
+
+
+    public static void add(double n1, double n2) {
+        System.out.println(n1 + n2);
+    }
+
+    public static void add(int n1, int n2) {
+        System.out.println(n1 + n2);
+    }
+
+    public static void subtract(int n1, int n2) {
+        System.out.println(n1 - n2);
+
+        /*
+        Output A?-->7.0
+        Output B?-->11
+        Output C?->15.0
+        Output D?-->-1
+        In what order does Java execute this code?
+         */
+    }
+
+    public int sumNumber(int a, int b){
+        return a+b;
+    }
+
+    public int sumNumber(int a, int b, int c){
+        return a+b+c;
+    }
+
+}
+

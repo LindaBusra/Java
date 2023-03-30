b6aae8c6946e7be63e4da1a22e3cfd048e49d1c6
LindaBusra <linda.busra.berg@gmail.com>
2023-03-30 23:12:09 +0200

    StringManipulations

---
+++ src/main/java/JavaLessons/day07_stringmanipulations/StringManipulations11.java
@@ -0,0 +1,47 @@
+package JavaLessons.day07_stringmanipulations;
+
+import java.util.Scanner;
+
+public class StringManipulations11 {
+    public static void main(String[] args) {
+
+
+
+
+String str = "Ali is 13 years old, I like Ali, Ali does not like me!...";
+
+//Example 1. How manye punctutions are used in the given String
+
+        System.out.println(str.replaceAll("[^\\p{Punct}]", "").length()  ); //6
+        System.out.println(str.replaceAll("\\P{Punct}", "").length()  ); //6
+
+//or
+
+        int numOfAllChars = str.length();
+        int numOfAllCharsDifFromPuncMarks = str.replaceAll("\\p{Punct}","").length();
+
+        System.out.println(numOfAllChars-numOfAllCharsDifFromPuncMarks); //6
+
+
+
+ //-------------------delete spaces---------------------------------
+
+
+
+//Example 2-Ask user to enter his full name, and fix if the user enters space
+//at the beginning and at the end.
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your first and last name");
+        String name = scan.nextLine();
+
+        String fixedFullName = name.trim();
+        System.out.println("Name without space: " + fixedFullName);
+
+
+
+
+
+    }
+}

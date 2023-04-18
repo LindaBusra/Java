d41f85eb123ded65050e9f93f5877d50b4e9e9bd
LindaBusra <linda.busra.berg@gmail.com>
2023-04-18 17:18:59 +0200

    Variables

---
+++ src/main/java/Batch159/day01_variables/Variables01.java
@@ -0,0 +1,30 @@
+package Batch159.day01_variables;
+
+public class Variables01 {
+
+    //How to create a variable
+
+    public int ageOfEmily = 23;  //This is a statement. (like a sentence). Age variable is int data type.  Camel case rules.
+    public String nameOfEmily = "EmilyNilsen";
+    //default value for int is 0, for String is null.
+    // = assignment operator, it takes value from right, and put in the container on the left
+
+    //Data types in Java
+    //There are 2 main data types; i)Primitive Data Types   ii)Non-primitive data types
+
+    /*
+    Primitive Data types:   boolean - char - byte - short - int - long - float - double
+                            byte - short - int - long - float - double  -->numeric data types
+                            boolean - char -->not numeric data types
+
+    boolean: boolean data types has 2 values --> True-False. Boolean uses just "1 bit" in the memory.
+     */
+
+    public boolean isRetired = true;
+
+    /*
+    Java has memory, and java splitter memory to small pieces. The smallest unit in memory calls "bit" In a bit
+    you can put just 1 or 0
+    1 byte : 8 bits
+     */
+}

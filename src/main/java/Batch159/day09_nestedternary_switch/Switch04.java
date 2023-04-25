ed0edf0234b9d4e101c36d9575323345a97ba912
LindaBusra <linda.busra.berg@gmail.com>
2023-04-25 09:00:59 +0200

    Switch

---
+++ src/main/java/Batch159/day09_nestedternary_switch/Switch04.java
@@ -0,0 +1,48 @@
+package Batch159.day09_nestedternary_switch;
+
+import java.util.Scanner;
+
+public class Switch04 {
+    public static void main(String[] args) {
+
+
+ //Switch Statement can be used with String, int, char but cannot be used with long, double, float, boolean
+
+//Example 1: Create a calculator does +, -, *, /, %
+
+        Scanner input= new Scanner(System.in);
+
+        System.out.println("Enter the operation  +, -, *, /, % ");
+
+        char opr= input.next().charAt(0);
+
+        System.out.println("Enter two numbers to do operation");
+        int a= input.nextInt();
+        int b= input.nextInt();
+
+
+        switch (opr) {
+
+            case '+':
+                System.out.println(a+ "+" + b + "=" + (a+b));
+                break;
+            case '-':
+                System.out.println(a+ "-" + b + "=" + (a-b));
+                break;
+            case '*':
+                System.out.println(a+ "*" + b + "=" + (a*b));
+                break;
+            case '/':
+                System.out.println(a+ "/" + b + "=" + (a/b));
+                break;
+            case '%':
+                System.out.println(a+ "%" + b + "=" + (a*b/100));
+                break;
+            default:
+                System.out.println("Undefined operation");
+        }
+
+
+
+    }
+}

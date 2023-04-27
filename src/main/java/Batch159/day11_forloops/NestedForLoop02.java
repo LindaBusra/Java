f7f9532c08c78b6ecdb96a8831d2e0df40aa3538
LindaBusra <linda.busra.berg@gmail.com>
2023-04-27 21:29:41 +0200

    NestedForLoop

---
+++ src/main/java/Batch159/day11_forloops/NestedForLoop02.java
@@ -0,0 +1,31 @@
+package Batch159.day11_forloops;
+
+public class NestedForLoop02 {
+    public static void main(String[] args) {
+
+
+
+/*   Example 1: Type code to get the output like
+                        1
+                        1 2
+                        1 2 3
+                        1 2 3 4
+                        1 2 3 4 5       */
+
+
+        for (int i=1; i<=5; i++) {
+
+            for(int j=1; j<=i; j++){
+
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+
+
+
+
+
+    }
+}

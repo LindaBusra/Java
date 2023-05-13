42687655f55540373b6cda1224847b4fb8db4a22
LindaBusra <linda.busra.berg@gmail.com>
2023-05-13 08:54:21 +0200

    Constructors

---
+++ src/main/java/Batch159/day23_constructors_accessmodifiers/StudentRunner.java
@@ -0,0 +1,12 @@
+package Batch159.day23_constructors_accessmodifiers;
+
+public class StudentRunner {
+
+    public static void main(String[] args) {
+
+
+        Student std1 = new Student();
+
+        System.out.println(std1.age);
+    }
+}

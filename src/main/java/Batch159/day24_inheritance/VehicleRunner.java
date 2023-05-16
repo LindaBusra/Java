5f00803bd348f18e689cababa07f24f35c4e90be
LindaBusra <linda.busra.berg@gmail.com>
2023-05-16 22:01:10 +0200

    Inheritance

---
+++ src/main/java/Batch159/day24_inheritance/VehicleRunner.java
@@ -0,0 +1,13 @@
+package Batch159.day24_inheritance;
+
+public class VehicleRunner {
+
+    public static void main(String[] args) {
+
+    Civic myCivic = new Civic();
+//    System.out.println(myCivic.age);        //if class has this value, it returns it. if it has not, it brings from Honda, 6
+
+
+        Civic myCivic1 = new Civic(5);  //it takes me to the constructor with int parameter in Civic class
+    }
+}

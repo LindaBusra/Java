71accc1f4f6ddef9f2aa0da77b09e6e5f3213c86
LindaBusra <linda.busra.berg@gmail.com>
2023-05-17 22:11:56 +0200

    Overriding

---
+++ src/main/java/Batch159/day25_inheritance_overriding/overriding/Mammal.java
@@ -0,0 +1,13 @@
+package Batch159.day25_inheritance_overriding.overriding;
+
+public class Mammal extends Animal{
+
+    //https://lms.techproeducation.com/mod/book/view.php?id=6589&chapterid=26509
+    //2.09
+
+    public int age =6;
+    public boolean old = true;
+    public char initial = 'A';
+
+
+}
